63e4bdbbdd2663ed0e91c47e6802da5f163fbf83
Morzhakov Kyrylo <morzhakov.kyrylo@gmail.com>
2022-10-26 18:39:33 +0300

    1

---
+++ src/main/java/com/hillel/morzhakov/lessons/lesson2/Main.java
@@ -0,0 +1,12 @@
+package com.hillel.morzhakov.lessons.lesson2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Eiffel Tower, Paris, France");
+        System.out.println("Geographic Information");
+        System.out.println("Latitude - 48.8534100 º");
+        System.out.println("Longitude - 2.294694 º");
+    }
+}
